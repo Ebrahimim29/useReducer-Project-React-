259b2c195871f49cc0989104581723839f369490
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-11-19 06:51:07 +0330

    useReducer-part3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer, //useState
+ } from "react";
 
 const initialState = { data: null, loading: true, error: null };
 
@@ -24,22 +25,28 @@ const App = () => {
   const [state, dispatch] = useReducer(dataReducer, initialState)
 
   useEffect(() => {
-    setLoading(true);
+    // setLoading(true);
+    dispatch({type:"FETCH_START"});
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => {
-        setData(data);
-        setLoading(false);
+        // setData(data);
+        // setLoading(false);
+        dispatch({type:"FETCH_SUCCESS", payload:data})
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(() =>
+        dispatch({type:"FETCH_ERROR" , payload:"خطا در گرفتن دیتا"}));
 
-        setError("خطا در گرفتن دیتا");
-        setLoading(false);
-      });
+      // .catch((err) => {
+        // console.log(err);
+
+        // setError("خطا در گرفتن دیتا");
+        // setLoading(false);
+      // });
   }, []);
 
-  if (loading)
+  if (state.loading)
     return (
       <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-blue-50 to-indigo-500">
         <div className="animate-pulse flex flex-col items-center">
@@ -52,13 +59,13 @@ const App = () => {
       </div>
     );
 
-  if (error)
+  if (state.error)
     return (
       <div className="min-h-screen flex items-center justify-center bg-red-200">
         <div className="text-center p-8 bg-white/35 rounded-lg shadow-lg border-2 border-red-400">
           <svg
-            xmlns="http://www.w3.org/200/svg"
-            className="h-16 w-16 text-red-300 mx-auto mb-4"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-16 w-16 text-red-500 mx-auto mb-4"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -67,10 +74,10 @@ const App = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M12 8v4m0 4h.01M21 12a9 9 0 11 0118 0z"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
             />
           </svg>
-          <p className="text-red-600 text-lg font-medium">{error}</p>
+          <p className="text-red-600 text-lg font-medium">{state.error}</p>
         </div>
       </div>
     );
@@ -81,7 +88,7 @@ const App = () => {
         لیست کاربران
       </h1>
       <div className="max-w-4xl mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {data.map((user) => (
+        {state.data.map((user) => (
           <div
             key={user.id}
             className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 border border-gray-100"
@@ -92,9 +99,24 @@ const App = () => {
             </div>
             <div className="p-6 space-y-3">
               <div className="flex items-center text-gray-700">
+                
                 <span>{user.email}</span>
               </div>
               <div className="flex items-center text-amber-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                  />
+                </svg>
                 <span>{user.phone}</span>
               </div>
               <div className="flex items-center text-pink-500">
@@ -110,4 +132,3 @@ const App = () => {
 
 export default App;
 
-// part:345- 6:00
